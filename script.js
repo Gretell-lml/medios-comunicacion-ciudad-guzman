4c4cff5a01247a9f38b51fbe2b9885edb0206a51
Gretell-lml <lomelilopezgretellitzel@gmail.com>
2026-06-01 17:41:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Página cargada correctamente");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".hidden").forEach((el)=>{
+    observer.observe(el);
+});
